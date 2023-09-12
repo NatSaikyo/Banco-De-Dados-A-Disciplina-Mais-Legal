6a89e377061bc3df37e2e1543eab1e20dfea5252
NatSaikyo <102774939+NatSaikyo@users.noreply.github.com>
2023-09-11 21:25:11 -0300

    EX03

--- listaDeExercicios01.sql
+++ listaDeExercicios01.sql
@@ -3,3 +3,8 @@ SELECT titulo FROM livros;
 
 --EX02
 SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
+
+--EX03
+SELECT  titulo ,autor_id, nome FROM livros l 
+JOIN autores a ON a.id = l.autor_id
+WHERE a.nome = 'J.K. Rowling';
