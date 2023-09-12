c8c9f98cc11ef72d4e76a91d79917d720431789b
NatSaikyo <102774939+NatSaikyo@users.noreply.github.com>
2023-09-11 21:25:25 -0300

    EX04

--- listaDeExercicios01.sql
+++ listaDeExercicios01.sql
@@ -8,3 +8,8 @@ SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
 SELECT  titulo ,autor_id, nome FROM livros l 
 JOIN autores a ON a.id = l.autor_id
 WHERE a.nome = 'J.K. Rowling';
+
+--EX04
+SELECT aluno_id, nome, curso FROM alunos a
+LEFT JOIN matriculas m ON m.id  = a.id
+WHERE curso = "Engenharia de Software";
