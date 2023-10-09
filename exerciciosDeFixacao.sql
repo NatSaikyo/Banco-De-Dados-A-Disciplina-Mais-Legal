629c96457336ee4b557be4c1c84f42e4e9a9e65e
NatSaikyo <102774939+NatSaikyo@users.noreply.github.com>
2023-10-08 22:53:36 -0300

    EX03

--- exerciciosDeFixacao.sql
+++ exerciciosDeFixacao.sql
@@ -49,3 +49,36 @@ FROM produtos;
 --D)
 SELECT AVG(preco) AS media_precos
 FROM produtos;
+
+--EX03
+--A)
+CREATE TABLE eventos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_evento DATETIME
+);
+
+INSERT INTO eventos (data_evento) VALUES
+('2018-10-15 10:10:10'),
+('2009-11-02 12:00:00'),
+('2000-11-20 11:59:59'),
+('1948-12-05 12:28:59'),
+('2024-01-10 06:09:00'),
+('1900-02-22 08:12:34'),
+('2070-03-15 15:15:30'),
+('2021-04-08 12:12:12'),
+('2020-05-19 23:59:10'),
+('2019-06-02 00:00:00'),
+('2118-07-14 00:12:00'),
+('2006-04-28 02:00:38');
+
+--B)
+INSERT INTO eventos (data_evento)
+SELECT NOW() as DatAtual;
+
+--C)
+SELECT data_evento, data_evento AS data_atual, DATEDIFF(NOW(), data_evento) AS Comparação_total
+FROM eventos;
+
+--D)
+SELECT DAYNAME(data_evento) AS nome_dia_semana
+FROM eventos;
